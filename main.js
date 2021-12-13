ca2a8e83352a4a9b7aae303a8b98b71cee886746
문현정 <munhyeonjeong@munhyeonjeong-ui-iMac.local>
2021-12-13 11:09:06 +0900

    Create project

---
+++ main.js
@@ -0,0 +1,10 @@
+const str = `
+010-1234-5678
+thesecon@gmail.com
+https://www.omdbapi.com/?apikey=7035c60c&s=frozen
+The quick brouwn fox jumps over the lazy dog.
+abbcccdddd
+`
+console.log(
+  str.match(/(?<=@).{1,}/g)
+)
